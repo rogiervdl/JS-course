ae63d1f99c3ca5d7e4b3a9968c7ce4bdf25a8fe6
Rogier van der Linde <rogier.vanderlinde@odisee.be>
2024-02-26 23:19:12 +0100

    dom deel 2

---
+++ demos/03_dom2/dom-navigation/js/scripts.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mainmenu');
+
+console.log(`menu's parent node:`);
+console.log(menu.parentNode);
+console.log(`menu's child nodes:`);
+console.log(menu.children);
+
+const nav = document.querySelector('nav');
+console.log(`nav's previous sibling:`);
+console.log(nav.previousElementSibling);
