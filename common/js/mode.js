c2fd57a8dccdcf4a90cafb477e824e344264ae61
Rogier van der Linde <rogier.vanderlinde@odisee.be>
2024-05-29 08:19:49 +0200

    codebase update

---
+++ common/js/mode.js
@@ -0,0 +1,9 @@
+// n00b/pro mode
+const chbMode = document.querySelector("#mode [type=checkbox]");
+chbMode.addEventListener('change', function() {
+   document.body.classList.toggle('showpro', this.checked);
+   localStorage.setItem('showpro', this.checked ? 'true' : 'false');
+});
+const showPro = localStorage.getItem('showpro') != 'false';
+document.body.classList.toggle('showpro', showPro);
+chbMode.checked = showPro;
