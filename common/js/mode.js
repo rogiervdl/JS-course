45ff8337489bbe0ac5e7a588560ceda9db9ab2ca
Rogier van der Linde <rogier.vanderlinde@odisee.be>
2025-02-11 19:40:38 +0100

    basisstructuur scripts toegevoegd ➕

--- common/js/mode.js
+++ common/js/mode.js
@@ -1,9 +1,25 @@
-// n00b/pro mode
-const chbMode = document.querySelector("#mode [type=checkbox]");
-chbMode.addEventListener('change', function() {
-   document.body.classList.toggle('showpro', this.checked);
-   localStorage.setItem('showpro', this.checked ? 'true' : 'false');
-});
+// n00b/pro mode checkbox
+const chbMode = document.querySelector('#mode [type=checkbox]');
+
+// handle change event
+const handleChangeEvent = () => {
+   document.body.classList.toggle('showpro', chbMode.checked);
+   localStorage.setItem('showpro', chbMode.checked ? 'true' : 'false');
+};
+chbMode.addEventListener('change', handleChangeEvent);
+
+// restore from localstorage
 const showPro = localStorage.getItem('showpro') != 'false';
 document.body.classList.toggle('showpro', showPro);
 chbMode.checked = showPro;
+
+// set mode with URL parameter
+const url = new URL(window.location.href);
+if (url.searchParams.has('pro') || url.searchParams.get('mode') === 'pro') {
+	chbMode.checked = true;
+	handleChangeEvent();
+}
+if (url.searchParams.has('n00b') || url.searchParams.get('mode') === 'n00b') {
+	chbMode.checked = false;
+	handleChangeEvent();
+}
